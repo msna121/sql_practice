734ee5ef045c083c57b140c4d7b0e9be65ed7a8d
MOHAMMED-SWALEH-NADEEM <53415113+MOHAMMED-SWALEH-NADEEM@users.noreply.github.com>
2021-10-11 18:08:19 +0530

    Assessment3 added

---
+++ Assessment3/assessment3.3.sql
@@ -0,0 +1,14 @@
+INSERT INTO students(
+	first_name,
+	last_name,
+	phone,
+	graduation_year,
+	homeroom_number
+)
+VALUES(
+	'Mark',
+	'Watney',
+	'7775551234',
+	2035,
+	5
+);
---
+++ Assessment3/assessment3.4.sql
@@ -0,0 +1,16 @@
+INSERT INTO teachers(
+	first_name,
+	last_name,
+	phone,
+	department,
+	homeroom_number,
+	email
+)
+VALUES(
+	'Jonas',
+	'Salk',
+	'7775554321',
+	'Biology',
+	5,
+	'jsalk@school.org'
+);
